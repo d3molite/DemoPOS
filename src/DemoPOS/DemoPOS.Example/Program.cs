e6d3534df95caa7eb8ffece8094cb8345d1d67cf
Demolite <tobi.brinke@gmail.com>
2024-06-17 22:55:43 +0200

    Added Example Project

---
+++ src/DemoPOS/DemoPOS.Example/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DemoPOS.Document;
+using DemoPOS.Helpers;
+
+
+var printer = RegistryHelper.GetInstalledPrinterNames()[1];
+
+var manager = new DocumentManager(printer);
+
+manager.AddTitle("Hello World")
+		.AddLine("This is a test print")
+		.AddLine("On a thermal printer.")
+		.AddQrCode("http://www.example.com")
+		.AddEmptyLines(5)
+		.Cut();
+
+manager.Print();
